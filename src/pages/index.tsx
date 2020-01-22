6af3173fa58f0c1b68a4877b38fb1cc8256f90e6
NissimBet <nissimbet123@outlook.com>
2020-01-21 18:00:36 -0600

    agregado image optimization

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import Head from 'next/head';
 
+import { Box } from '@material-ui/core';
+
 const Index = () => {
   return (
     <React.Fragment>
@@ -8,6 +10,9 @@ const Index = () => {
         <title>home</title>
       </Head>
       <p>hello</p>
+      <Box>
+        <p>hi</p>
+      </Box>
     </React.Fragment>
   );
 };
